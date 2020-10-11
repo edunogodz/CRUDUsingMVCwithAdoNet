6b29de51b882ef2327aafb3f1a4045fb9f060b23
carlos <edunogodz@gmail.com>
2020-10-11 16:16:16 -0300

    Finish the Tutorial

---
+++ CRUDUsingMVCwithAdoNet/Models/AbstractRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace CRUDUsingMVCwithAdoNet.Models
+{
+    public abstract class AbstractRepository<TEntity, TKey>
+            where TEntity : class
+    {
+        protected string StringConnection { get; } = WebConfigurationManager.ConnectionStrings["PodioAspnetSampleDb"].ConnectionString;
+
+        public abstract List<TEntity> GetAll();
+        public abstract TEntity GetById(TKey id);
+        public abstract void Save(TEntity entity);
+        public abstract void Update(TEntity entity);
+        public abstract void Delete(TEntity entity);
+        public abstract void DeleteById(TKey id);
+    }
+}
